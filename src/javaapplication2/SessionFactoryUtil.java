48e1172ececb65e5e225e17cc2757db878a7a2de
nuwan600 <nuwan600@gmail.com>
2014-10-14 13:18:57 +0530

    Update SessionFactoryUtil.java

--- src/javaapplication2/SessionFactoryUtil.java
+++ src/javaapplication2/SessionFactoryUtil.java
@@ -36,4 +36,35 @@ public class SessionFactoryUtil {
         return sessionFactory.getCurrentSession();
     }
     
+    /**
+* Opens a session and will not bind it to a session context
+* @return the session
+*/
+public static Session openSession() {
+return sessionFactory.openSession();
+}
+/**
+* Returns a session from the session context.
+* If there is no session in the context it opens a session,
+* stores it in the context and returns it.
+* This factory is intended to be used with a hibernate.cfg.xml
+* including the following property <property
+* name="current_session_context_class">thread</property>
+* This would return
+* the current open session or if this does not exist, will create a new
+* session
+*
+* @return the session
+*/
+public static Session getCurrentSession() {
+return sessionFactory.getCurrentSession();
+}
+/**
+* closes the session factory
+*/
+public static void close(){
+if (sessionFactory != null)
+sessionFactory.close();
+}
+    
 }
